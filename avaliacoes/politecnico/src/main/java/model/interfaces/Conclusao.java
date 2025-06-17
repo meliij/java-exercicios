63acb47985ba4e99ca67c570dc5ff8bd11351f62
Melissa <melissajqt18@gmail.com>
2025-06-17 17:56:16 -0300

    Javadoc politecnico

--- avaliacoes/politecnico/src/main/java/model/interfaces/Conclusao.java
+++ avaliacoes/politecnico/src/main/java/model/interfaces/Conclusao.java
@@ -1,12 +1,23 @@
 package model.interfaces;
-
+/**
+ * Importa bibliotecas e classes
+ */
 import java.util.ArrayList;
 
 import model.Disciplina;
 
+/**
+ * Representa a interface conclusão
+ */
 public interface Conclusao {
 
+    /**
+     * Método que verifica se o aluno está integralizado
+     */
     public boolean estaIntegralizado();
+    /**
+     * Método que retorna as disciplinas que o aluno ainsa não cursou
+     */
     public ArrayList<Disciplina> getDisciplinasFaltantes();
 
 }
