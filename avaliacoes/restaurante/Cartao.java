c70940bbf74a9171ebb2f751abd6291c8963bd14
meliij <melissajqt18@gmail.com>
2025-06-23 18:31:29 -0300

    Adicionada primeiras classes

---
+++ avaliacoes/restaurante/Cartao.java
@@ -0,0 +1,12 @@
+package restaurante.cefet;
+
+public class Cartao extends Pagamento {
+    public void pagar() {
+        
+    }
+    
+    public String getTipo()
+    {
+        return "CARTÃO";
+    }
+}
---
+++ avaliacoes/restaurante/Especie.java
@@ -0,0 +1,12 @@
+package restaurante.cefet;
+
+public class Especie extends Pagamento {
+    public void pagar() {
+        
+    }
+    
+    public String getTipo()
+    {
+        return "ESPÉCIE";
+    }
+}
